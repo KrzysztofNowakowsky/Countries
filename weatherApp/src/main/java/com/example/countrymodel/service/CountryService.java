d0e9d3dae61934f50bf60dc87b9166d12484f799
Krzysztof Nowakowski <klnb2014@gmail.com>
2025-04-25 11:53:44 +0200

    fix (service) handle null from REST API properly

--- weatherApp/src/main/java/com/example/countrymodel/service/CountryService.java
+++ weatherApp/src/main/java/com/example/countrymodel/service/CountryService.java
@@ -4,6 +4,9 @@ import com.example.countrymodel.model.CountryInfo;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+import java.util.Map;
+
 @Service
 public class CountryService {
 
@@ -12,15 +15,21 @@ public class CountryService {
     public CountryInfo getCountry(String countryName) {
         String url = "https://restcountries.com/v3.1/name/" + countryName;
         var response = restTemplate.getForObject(url, Object[].class);
-        //var countryData = (Map<String, Object>) response[0];
-        System.out.println(response);
-        CountryInfo info = new CountryInfo();
-        return info;
-        // info.setName(((Map<String, String>) countryData.get("name")).get("common"));
-        // info.setCapital(((List<String>) countryData.get("capital")).get(0));
-        // info.setRegion((String) countryData.get("region"));
-        // info.setPopulation(((Number) countryData.get("population")).longValue());
+        if (response != null && response.length > 0 && response[0] instanceof Map) {
+            @SuppressWarnings("unchecked")
+            Map<String, Object> countryData = (Map<String, Object>) response[0];
 
-        // return info;
+            Map<String, Object> nameData = (Map<String, Object>) countryData.get("name");
+            List<String> capitals = (List<String>) countryData.get("capital");
+
+            CountryInfo country = new CountryInfo();
+            country.setName((String) nameData.get("common"));
+            country.setCapital(capitals.get(0));
+            country.setPopulation(((Number) countryData.get("population")).longValue());
+            country.setRegion((String) countryData.get("region"));
+
+            return country;
+        }
+        return null;
     }
 }
